b32941b59ce121934b660caaea2f2ca057bd304f
Cori Fowler <fowler.cori@gmail.com>
2015-11-04 17:16:32 -0500

    added a bunch of files + router

---
+++ js/router.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import Backbone from 'backbone';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default Backbone.Router.extend({
+
+	routes: {
+		"" : "home",
+		"register" : "registerForm",
+		"login" : "userLogin",
+		"deckgallery" : "viewDecks",
+		"flashgame" : "playGame"
+	}
+
+
+});
