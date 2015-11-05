a66e22db84fb2be5cfe0c2e8d2dd799452c67061
Jef Blocker <jefblocker@gmail.com>
2015-11-05 14:49:10 -0500

    Completed adding form for adding deck to add deck view.

--- js/views/add_view.js
+++ js/views/add_view.js
@@ -12,7 +12,7 @@ export default React.createClass({
     this.props.onCreateClick();
   },
 
-  // Back to Deck Callery
+  // Back to Deck Gallery
 
   onBackClick() {
     this.props.onBackClick();
@@ -22,18 +22,19 @@ export default React.createClass({
   render() {
     return (
       <div className='wholePage'>
+      // Create form for deck title and every question and and answer for the five (5) cards in the deck
         <form className='form'>
           <label>Deck Title<input type='text' placeholder='Enter deck title' className='deckTitle' maxlength="255" /></label>
-          <label>Question 1 <input type='text' placeholder='' className='question' maxlength="255" /></label>
-          <label>Answer 1<input type='text' placeholder='' className='answer' maxlength="255" /></label>
-          <label>Question 2 <input type='text' placeholder='' className='question' maxlength="255" /></label>
-          <label>Answer 2<input type='text' placeholder='' className='answer' maxlength="255" /></label>
-          <label>Question 3<input type='text' placeholder='' className='question' maxlength="255" /></label>
-          <label>Answer 3<input type='text' placeholder='' className='answer' maxlength="255" /></label>
-          <label>Question 4<input type='text' placeholder='' className='question' maxlength="255" /></label>
-          <label>Answer 4<input type='text' placeholder='' className='answer' maxlength="255" /></label>
-          <label>Question 5<input type='text' placeholder='' className='question' maxlength="255" /></label>
-          <label>Answer 5<input type='text' placeholder='' className='answer' maxlength="255" /></label>
+          <label>Question 1 <input type='text' placeholder='Enter question 1' className='question1' maxlength="255" /></label>
+          <label>Answer 1<input type='text' placeholder='Enter answer 1' className='answer1' maxlength="255" /></label>
+          <label>Question 2 <input type='text' placeholder='Enter question 2' className='question2' maxlength="255" /></label>
+          <label>Answer 2<input type='text' placeholder='Enter answer 2' className='answer2' maxlength="255" /></label>
+          <label>Question 3<input type='text' placeholder='Enter question 3' className='question3' maxlength="255" /></label>
+          <label>Answer 3<input type='text' placeholder='Enter answer 3' className='answer3' maxlength="255" /></label>
+          <label>Question 4<input type='text' placeholder='Enter question 4' className='question4' maxlength="255" /></label>
+          <label>Answer 4<input type='text' placeholder='Enter answer 4' className='answer4' maxlength="255" /></label>
+          <label>Question 5<input type='text' placeholder='Enter question 5' className='question5' maxlength="255" /></label>
+          <label>Answer 5<input type='text' placeholder='Enter answer 5' className='answer5' maxlength="255" /></label>
          <button onClick={this.onCreateClick} className='createBtn'>Create Deck</button>
         </form>
 
