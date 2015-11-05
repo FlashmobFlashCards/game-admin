45b542210a1630f24cb5349f38f2cf86203d1e98
Hyona Kim <tekkieninja@gmail.com>
2015-11-05 17:00:34 -0500

    register button working

--- js/views/choose_deck_view.js
+++ js/views/choose_deck_view.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+// import React from 'react';
+// import ReactDom from 'react-dom';
 
-export default React.createClass({
-// Display deck titles associated with user ID.
+// export default React.createClass({
+// // Display deck titles associated with user ID.
 
-// Click on deck title to go to associated edit_card_view.
+// // Click on deck title to go to associated edit_card_view.
 
-  getInitialState: function() {
-    return {value: ''};
-  },
+//   getInitialState: function() {
+//     return {value: ''};
+//   },
 
-  handleChange: function(event) {
-    this.setState({value: event.target.value});
-  },
+//   handleChange: function(event) {
+//     this.setState({value: event.target.value});
+//   },
 
-  render: function() {
-    var value = this.state.value;
-    return <input type="text" value={value} onChange={this.handleChange} /;
-  }
+//   render: function() {
+//     var value = this.state.value;
+//     return <input type="text" value={value} onChange={this.handleChange} />;
+//   }
 
-  // Create event listeners for editing deck fields
+//   // Create event listeners for editing deck fields
 
-  onEditClick() {
-    this.props.onEditClick();
-  },
+//   onEditClick() {
+//     this.props.onEditClick();
+//   },
 
-    onBackClick() {
-    this.props.onBackClick();
-  },  
+//     onBackClick() {
+//     this.props.onBackClick();
+//   },  
 
 
-  render() {
-    return (
-      <div className='wholePage'>
+//   render() {
+//     return (
+//       <div className='wholePage'>
 
-      // Create button to save changes
-        <button onClick={this.onBackClick} className='backBtn'>Back to Deck Gallery</button>
-      </div>
-    );
-  }
+//       // Create button to save changes
+//         <button onClick={this.onBackClick} className='backBtn'>Back to Deck Gallery</button>
+//       </div>
+//     );
+//   }
 
-});
+// });
