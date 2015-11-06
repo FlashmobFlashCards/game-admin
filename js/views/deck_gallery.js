4ce9ffd7142220c0e3054c69cdc16f18776ffea1
Jef Blocker <jefblocker@gmail.com>
2015-11-06 10:59:04 -0500

    Added react code to edit_card.js

--- js/views/deck_gallery.js
+++ js/views/deck_gallery.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+// import React from 'react';
+// import ReactDom from 'react-dom';
 
-export default React.createClass({
+// export default React.createClass({
 
-  onPlayClick() {
-    this.props.onPlayClick();
-  },
+//   onPlayClick() {
+//     this.props.onPlayClick();
+//   },
 
-  onAddClick() {
-    this.props.onAddClick();
-  },
+//   onAddClick() {
+//     this.props.onAddClick();
+//   },
 
-    onEditClick() {
-    this.props.onEditClick();
-  },  
+//     onEditClick() {
+//     this.props.onEditClick();
+//   },  
 
 
-  render() {
-    return (
-      <div className='wholePage'>
-        <button onClick={this.onPlayClick} className='playBtn'>Play</button>
-        <button onClick={this.onAddClick} className='addBtn'>Add a Deck</button>
-        <button onClick={this.onEditClick} className='edityBtn'>Edit a Deck</button>
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div className='wholePage'>
+//         <button onClick={this.onPlayClick} className='playBtn'>Play</button>
+//         <button onClick={this.onAddClick} className='addBtn'>Add a Deck</button>
+//         <button onClick={this.onEditClick} className='edityBtn'>Edit a Deck</button>
+//       </div>
+//     );
+//   }
 
-});
+// });
--- js/views/edit_card_view.js
+++ js/views/edit_card_view.js
@@ -3,47 +3,70 @@ import ReactDom from 'react-dom';
 
 export default React.createClass({
 
-  // Need to be able to choose deck ID from list associated with user ID.
+// DISPLAY ALL CARD_IDS FOR SELECTED DECK_ID
+// GET DECK_ID TO SHOW CARDS
+// DELETE CARD
+// ADD CARD
+// EDIT QUESTION AND/OR ANSWER ON CARD
 
-  // Need to be able to edit deck title.
-
-  // Need to be able to edit question and answer on every card.
+// GET DECK ID, TITLE, CARD ID, QUESTION, AND ANSWER.
 
   getInitialState: function() {
-    return {value: ''};
+    return ({  // Is ( necessary?
+      deck_id: this.props.stored.deck_id, // Do we need to include stored?
+      title: this.props.stored.title,
+      card_id: this.props.stored.card_id,
+      question: this.props.stored.question,
+      answer: this.props.stored.answer 
+     }); // Is ) necessary?
   },
 
-  handleChange: function(event) {
-    this.setState({value: event.target.value});
+// UPDATE QUESTION
+
+  updateQuestion(e) {
+    let newQuestion = e.currentTarget.value;
+
+    this.setState({question: newQuestion});
   },
 
-  render: function() {
-    var value = this.state.value;
-    return <input type="text" value={value} onChange={this.handleChange} /;
-  }
+// UPDATE ANSWER
 
-  // Create event listeners for editing deck fields
+  updateAnswer(e) {
+    let newAnswer = e.currentTarget.value;
 
-  onEditClick() {
-    this.props.onEditClick();
-  },
-
-    onSaveClick() {
-    this.props.onSaveClick();
+    this setState({answer: newAnswer});
   },  
 
+// SAVE CHANGES
+
+  saveChanges(e) { // Does saveChanges need to be addChanges?
+    event.preventDefault();
+    this.props.onSubmitChangesClick(
+      this.state.question,
+      this.state.answer
+      );
+  },  
+
+// GO BACK TO EDIT DECK VIEW
+
+  goEditDeckView() {
+    this.props.onBackClick();
+  },
+
+// GO BACK TO DECK GALLERY
+
+  goDeckGalleryView() {
+    this.props.onBackClick(); // Does this need to be an individual onBackClick?
+  },
 
   render() {
     return (
-      <div className='wholePage'>
 
-      // Create button edit deck
-        <button onClick={this.onEditClick} className='editBtn'>Edit a Deck</button>
-
-      // Create button to save changes
-        <button onClick={this.onSaveClick} className='addBtn'>Save Changes</button>
+//  BUTTONS    
+      <div className="btns">
+        <button onClick{() => this.goEditDeckView()}>Edit Deck</button> // Do we need a fa fa icon for this?
+        <button onClick{() => this.goDeckGalleryView()}>Deck Gallery</button> // Do we need a fa fa icon for this?
+// Do we redirect to refreshed edit_card_view.js?        <button onClick{() => this.go?View()}>Save Changes</button> // Do we need a fa fa icon for this?
       </div>
-    );
-  }
-
-});
+      <div className="edit-card">
+      </div>
