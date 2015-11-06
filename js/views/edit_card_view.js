611835ad177e24e6ffc6fb6fd2b255a8c08025bc
Cori Fowler <fowler.cori@gmail.com>
2015-11-06 15:19:14 -0500

    able to add new deck

--- js/views/edit_card_view.js
+++ js/views/edit_card_view.js
@@ -1,120 +1,119 @@
-// import React from 'react';
-// import ReactDom from 'react-dom';
+// // import React from 'react';
+// // import ReactDom from 'react-dom';
 
-// export default React.createClass({
+// // export default React.createClass({
 
 
-//   // Need to be able to choose deck ID from list associated with user ID.
+// //   // Need to be able to choose deck ID from list associated with user ID.
 
-//   // Need to be able to edit deck title.
+// //   // Need to be able to edit deck title.
 
-//   // Need to be able to edit question and answer on every card.
+// //   // Need to be able to edit question and answer on every card.
+
+// //   getInitialState: function() {
+// //     return {value: ''};
+// //   },
+
+// //   handleChange: function(event) {
+// //     this.setState({value: event.target.value});
+// //   },
+
+// //   render: function() {
+// //     var value = this.state.value;
+// //     return <input type="text" value={value} onChange={this.handleChange} />;
+// //   }
+
+// //   // Create event listeners for editing deck fields
+
+// //   onEditClick() {
+// //     this.props.onEditClick();
+// //   },
+
+// //   onSaveClick() {
+// //     this.props.onSaveClick();
+// //   },  
+// // DISPLAY ALL CARD_IDS FOR SELECTED DECK_ID
+// // GET DECK_ID TO SHOW CARDS
+// // DELETE CARD
+// // ADD CARD
+// // EDIT QUESTION AND/OR ANSWER ON CARD
+
+// // GET DECK ID, TITLE, CARD ID, QUESTION, AND ANSWER.
 
 //   getInitialState: function() {
-//     return {value: ''};
+//     return ({  // Is ( necessary?
+//       deck_id: this.props.stored.deck_id, // Do we need to include stored?
+//       title: this.props.stored.title,
+//       card_id: this.props.stored.card_id,
+//       question: this.props.stored.question,
+//       answer: this.props.stored.answer 
+//      }); // Is ) necessary?
 //   },
 
-//   handleChange: function(event) {
-//     this.setState({value: event.target.value});
+// // UPDATE QUESTION
+
+//   updateQuestion(e) {
+//     let newQuestion = e.currentTarget.value;
+
+//     this.setState({question: newQuestion});
 //   },
 
-//   render: function() {
-//     var value = this.state.value;
-//     return <input type="text" value={value} onChange={this.handleChange} />;
-//   }
+// // UPDATE ANSWER
 
-//   // Create event listeners for editing deck fields
+//   updateAnswer(e) {
+//     let newAnswer = e.currentTarget.value;
 
-//   onEditClick() {
-//     this.props.onEditClick();
-//   },
-
-//   onSaveClick() {
-//     this.props.onSaveClick();
+//     this setState({answer: newAnswer});
 //   },  
-=======
-// DISPLAY ALL CARD_IDS FOR SELECTED DECK_ID
-// GET DECK_ID TO SHOW CARDS
-// DELETE CARD
-// ADD CARD
-// EDIT QUESTION AND/OR ANSWER ON CARD
 
-// GET DECK ID, TITLE, CARD ID, QUESTION, AND ANSWER.
+// // SAVE CHANGES
 
-  getInitialState: function() {
-    return ({  // Is ( necessary?
-      deck_id: this.props.stored.deck_id, // Do we need to include stored?
-      title: this.props.stored.title,
-      card_id: this.props.stored.card_id,
-      question: this.props.stored.question,
-      answer: this.props.stored.answer 
-     }); // Is ) necessary?
-  },
+//   saveChanges(e) { // Does saveChanges need to be addChanges?
+//     event.preventDefault();
+//     this.props.onSubmitChangesClick(
+//       this.state.question,
+//       this.state.answer
+//       );
+//   },  
+// >>>>>>> jef
 
-// UPDATE QUESTION
+// // GO BACK TO EDIT DECK VIEW
 
-  updateQuestion(e) {
-    let newQuestion = e.currentTarget.value;
+//   goEditDeckView() {
+//     this.props.onBackClick();
+//   },
 
-    this.setState({question: newQuestion});
-  },
+// // GO BACK TO DECK GALLERY
 
-// UPDATE ANSWER
+//   goDeckGalleryView() {
+//     this.props.onBackClick(); // Does this need to be an individual onBackClick?
+//   },
 
-  updateAnswer(e) {
-    let newAnswer = e.currentTarget.value;
 
-    this setState({answer: newAnswer});
-  },  
+// //   render() {
+// //     return (
+// //       <div className='wholePage'>
 
-// SAVE CHANGES
+// //       // Create button edit deck
+// //         <button onClick={this.onEditClick} className='editBtn'>Edit a Deck</button>
 
-  saveChanges(e) { // Does saveChanges need to be addChanges?
-    event.preventDefault();
-    this.props.onSubmitChangesClick(
-      this.state.question,
-      this.state.answer
-      );
-  },  
->>>>>>> jef
+// //       // Create button to save changes
+// //         <button onClick={this.onSaveClick} className='addBtn'>Save Changes</button>
+// //       </div>
+// //     );
+// //   }
 
-// GO BACK TO EDIT DECK VIEW
+// // });
 
-  goEditDeckView() {
-    this.props.onBackClick();
-  },
-
-// GO BACK TO DECK GALLERY
-
-  goDeckGalleryView() {
-    this.props.onBackClick(); // Does this need to be an individual onBackClick?
-  },
-
-<<<<<<< HEAD
 //   render() {
 //     return (
-//       <div className='wholePage'>
 
-//       // Create button edit deck
-//         <button onClick={this.onEditClick} className='editBtn'>Edit a Deck</button>
-
-//       // Create button to save changes
-//         <button onClick={this.onSaveClick} className='addBtn'>Save Changes</button>
+// //  BUTTONS    
+//       <div className="btns">
+//         <button onClick{() => this.goEditDeckView()}>Edit Deck</button> // Do we need a fa fa icon for this?
+//         <button onClick{() => this.goDeckGalleryView()}>Deck Gallery</button> // Do we need a fa fa icon for this?
+// // Do we redirect to refreshed edit_card_view.js?        <button onClick{() => this.go?View()}>Save Changes</button> // Do we need a fa fa icon for this?
 //       </div>
-//     );
-//   }
+//       <div className="edit-card">
+//       </div>
 
-// });
-=======
-  render() {
-    return (
-
-//  BUTTONS    
-      <div className="btns">
-        <button onClick{() => this.goEditDeckView()}>Edit Deck</button> // Do we need a fa fa icon for this?
-        <button onClick{() => this.goDeckGalleryView()}>Deck Gallery</button> // Do we need a fa fa icon for this?
-// Do we redirect to refreshed edit_card_view.js?        <button onClick{() => this.go?View()}>Save Changes</button> // Do we need a fa fa icon for this?
-      </div>
-      <div className="edit-card">
-      </div>
->>>>>>> jef
--- js/views/index.js
+++ js/views/index.js
@@ -3,6 +3,7 @@ import HomeView from './home';
 import LoginView from './login_page';
 import CreateCard from './create_card';
 import UserHomeView from './deck_gallery';
+import CreateDeck from './create_deck';
 
 
 export {
@@ -10,5 +11,6 @@ export {
 	RegisterForm,
 	LoginView,
 	CreateCard,
+	CreateDeck,
 	UserHomeView
 };
