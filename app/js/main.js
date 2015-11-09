73a1e45731f9f84e0bb1370bfa19bb5097054934
Cori Fowler <fowler.cori@gmail.com>
2015-11-08 22:02:12 -0500

    cleaned up files

--- app/js/main.js
+++ app/js/main.js
@@ -1118,7 +1118,6 @@ exports['default'] = _react2['default'].createClass({
   displayName: 'register',
 
   createUser: function createUser() {
-    console.log('this is clicked');
     this.props.onCreateUserClick();
   },
 
@@ -1251,46 +1250,6 @@ exports["default"] = _react2["default"].createClass({
 	}
 
 });
-
-// export default React.createClass({
-// // Display deck titles associated with user ID.
-
-// // Click on deck title to go to associated edit_card_view.
-
-//   getInitialState: function() {
-//     return {value: ''};
-//   },
-
-//   handleChange: function(event) {
-//     this.setState({value: event.target.value});
-//   },
-
-//   render: function() {
-//     var value = this.state.value;
-//     return <input type="text" value={value} onChange={this.handleChange} />;
-//   }
-
-//   // Create event listeners for editing deck fields
-
-//   onEditClick() {
-//     this.props.onEditClick();
-//   },
-
-//     onBackClick() {
-//     this.props.onBackClick();
-//   }, 
-
-//   render() {
-//     return (
-//       <div className='wholePage'>
-
-//       // Create button to save changes
-//         <button onClick={this.onBackClick} className='backBtn'>Back to Deck Gallery</button>
-//       </div>
-//     );
-//   }
-
-// });
 module.exports = exports["default"];
 
 },{"react":180}],19:[function(require,module,exports){
